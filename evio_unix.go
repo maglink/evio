ca40fe9b256135d2597c8daab7d737f6fb23ea15
maglink <maglink@yandex.ru>
2019-11-14 15:40:16 +0300

    Fix path to new fork

--- evio_unix.go
+++ evio_unix.go
@@ -17,8 +17,8 @@ import (
 	"time"
 
 	reuseport "github.com/kavu/go_reuseport"
-	"github.com/tidwall/evio/internal"
-	"github.com/tidwall/evio/internal/affinity"
+	"github.com/maglink/evio/internal"
+	"github.com/maglink/evio/internal/affinity"
 )
 
 type conn struct {
